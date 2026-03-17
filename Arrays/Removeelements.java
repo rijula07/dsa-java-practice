bf93f276cd66262b470a3a7835797b57becb72e5
Rijula Jaitly <rijula.art07@gmail.com>
2026-03-17 17:04:37 +0530

    Added Remove elements solution

---
+++ Arrays/Removeelements.java
@@ -0,0 +1,16 @@
+// Problem: LeetCode 27 - Remove Element
+// Approach: Two Pointer Technique 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k =0;
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i]!=val) {
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
