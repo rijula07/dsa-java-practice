96c1dcfd8d993723692b21dc7ba5c3eeff8a1016
Rijula Jaitly <rijula.art07@gmail.com>
2026-03-28 18:14:55 +0530

    Added leetcode 53 solution

---
+++ Arrays/leetcode53.java
@@ -0,0 +1,17 @@
+// Problem: LeetCode 53 - Maximum Subarray
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+       int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+} 
+    
