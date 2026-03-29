d00e03f1fd45ec7295badcb25cb2ff4654d0a3a1
Rijula Jaitly <rijula.art07@gmail.com>
2026-03-29 14:34:24 +0530

    Added leetcode 344 solution

---
+++ Strings/leetcode344.java
@@ -0,0 +1,16 @@
+// Problem: LeetCode 344 - Reverse String
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public void reverseString(char[] s) {
+      int left = 0;
+      int right = s.length-1;
+      while(left<right) {
+        char temp = s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        left++;
+        right--;
+      } 
+    }
+}
