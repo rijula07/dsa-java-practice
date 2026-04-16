4892ccbf30de8e514503b3c5a96e3e5e2f4660c1
Rijula Jaitly <rijula.art07@gmail.com>
2026-04-16 23:49:25 +0530

    Added leetcode 14 solution

--- Arrays/leetcode14.java
+++ Arrays/leetcode14.java
@@ -4,7 +4,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
-
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
             while (!strs[i].startsWith(prefix)) {
